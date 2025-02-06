a29260b7e653c82f32401807bb04b46b55a15545
Santiago Bergerat <santiagobergerat@gmail.com>
2025-02-05 23:43:46 -0300

    starting team work

---
+++ week05/lists-numbers.py
@@ -0,0 +1,13 @@
+points_scored = [ 24,4,23,76,43,23,90]
+
+total_points=0
+
+for point in points_scored:
+    total_points += point
+    print(total_points)
+    
+    
+print(f'La suma total de los puntos es : {total_points}')
+    
+# for i, color in enumerate(colors):
+#     print(f"{i} - {color}")
--- week05/usingLoopsLists.py
+++ week05/usingLoopsLists.py
@@ -3,7 +3,8 @@ new_client = ""
 
 while new_client != "Quite":
     new_client = input("Name new clien: ").capitalize()
-    clients.append(new_client)
+    if new_client != "Quite":
+         clients.append(new_client)
     
     
 print( "The new clients are: ")
